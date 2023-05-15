233751699598c4302fe9f8a1ecf4aab55195c1ec
Dannie <dannienctu@gmail.com>
2023-05-15 16:58:26 -0700

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,13 +3,13 @@ import Globe from "./Globe";
 
 function App() {
   return (
-    <div>      
+    <div>
       {/*https://www.youtube.com/watch?v=vM8M4QloVL0*/}
-      <div className="flex h-screen">
-        <div className="w-2/3" id="canvasContainer">
+      <div className="flex flex-col md:flex-row h-screen">
+        <div className="md:w-2/3 flex-shrink-0" id="canvasContainer">
           <Globe />
         </div>
-        <div className="w-1/3" id="imageContainer">
+        <div className="md:w-1/3 flex-shrink-0" id="imageContainer">
           <h1></h1>
         </div>
       </div>
